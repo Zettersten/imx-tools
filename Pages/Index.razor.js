0e26a4c8659e8212e11de4b32b55e6c3ff6de573
Erik Zettersten <erik@veefriends.com>
2022-11-29 23:06:29 -0500

    adds beta-male tom shit

--- Pages/Index.razor.js
+++ Pages/Index.razor.js
@@ -1,44 +1,54 @@
-const starkContractAddress = "0xa3AEe8BcE55BEeA1951EF834b99f3Ac60d1ABeeB";
-const tokenAddress = "0xac98d8d1bb27a94e79fbf49198210240688bb1ed";
+let starkContractAddress = "0xa3AEe8BcE55BEeA1951EF834b99f3Ac60d1ABeeB";
+let tokenAddress = "0xac98d8d1bb27a94e79fbf49198210240688bb1ed";
+let publicApiUrl = "https://api.x.immutable.com/v1";
+let linkFrameUrl = "https://link.x.immutable.com";
+
+if (location.href.includes('?beta=true')) {
+    console.info("beta mode")
+    starkContractAddress = "0x7917eDb51ecD6CdB3F9854c3cc593F33de10c623";
+    tokenAddress = "0x1C97Ada273C9A52253f463042f29117090Cd7D83";
+    publicApiUrl = "https://api.sandbox.x.immutable.com/v1";
+    linkFrameUrl = "https://link.sandbox.x.immutable.com";
+}
 
 export async function init(walletAddress) {
-  const client = await getClient();
+    const client = await getClient();
 
-  const assets = await client.getAssets({
-    user: walletAddress,
-    page_size: 200,
-    order_by: "desc"
-  });
+    const assets = await client.getAssets({
+        user: walletAddress,
+        page_size: 200,
+        order_by: "desc"
+    });
 
-  return assets.result;
+    return assets.result;
 }
 
 export async function transfer(assetIds, toAddress) {
-  const client = await getLink();
+    const client = await getLink();
 
-  const payload = assetIds.map(assetId => {
-    return {
-      type: imx.ERC721TokenType.ERC721, // Must be of type ERC721
-      tokenId: assetId, // the token ID
-      tokenAddress: tokenAddress, // the collection address / contract address this token belongs to
-      toAddress: toAddress // the wallet address this token is being transferred to
-    };
-  });
+    const payload = assetIds.map(assetId => {
+        return {
+            type: imx.ERC721TokenType.ERC721, // Must be of type ERC721
+            tokenId: assetId, // the token ID
+            tokenAddress: tokenAddress, // the collection address / contract address this token belongs to
+            toAddress: toAddress // the wallet address this token is being transferred to
+        };
+    });
 
-  const assets = await client.batchNftTransfer(payload);
+    const assets = await client.batchNftTransfer(payload);
 
-  console.log(assets);
+    console.log(assets);
 
-  return assets;
+    return assets;
 }
 
 async function getClient() {
-  return await imx.ImmutableXClient.build({
-    publicApiUrl: `https://api.x.immutable.com/v1`,
-    starkContractAddress: starkContractAddress
-  });
+    return await imx.ImmutableXClient.build({
+        publicApiUrl: publicApiUrl,
+        starkContractAddress: starkContractAddress
+    });
 }
 
 function getLink() {
-  return new imx.Link("https://link.x.immutable.com");
+    return new imx.Link(linkFrameUrl);
 }
